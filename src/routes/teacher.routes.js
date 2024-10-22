5ac59b23a6db3323021c8c6578d5c77a886ce7c5
Aikant Kumar <kr.ashok0786@gmail.com>
2024-10-22 19:51:11 +0530

    added notification routes

--- src/routes/teacher.routes.js
+++ src/routes/teacher.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {registerTeacher,getAllEnrollments } from "../controllers/teacher.controller.js"
+import {sendNotification , getAllNotifications} from "../controllers/notification.controller.js"
 
 const router = Router()
 
@@ -14,5 +15,9 @@ router.route("/register-teacher").post(
     ]), registerTeacher)
 
 router.route("/get-enrollments").get(getAllEnrollments)
+
+router.route("/notification").post(sendNotification).get(getAllNotifications)
+
+
 
 export default router    
